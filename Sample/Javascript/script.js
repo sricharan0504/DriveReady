70b94fb43c63b799b19a96c0fd296799ec900577
sricharan0504 <sricharan0504@gmail.com>
2024-07-02 09:43:21 +0530

    Commit

--- Sample/Javascript/script.js
+++ Sample/Javascript/script.js
@@ -1,10 +1,25 @@
-var x = 10;
-var y = 20;
-if (x > y){
-    document.write(x);
-    console.log(x);
-}
-else {
-    document.write(y);
-    console.log(y);
-}
+// // var x = 10;
+// // var y = 20;
+// // if (x > y){
+// //     document.write(x);
+// //     console.log(x);
+// // }
+// // else {
+// //     document.write(y);
+// //     console.log(y);
+// // }
+
+// var a = [10,11,12];
+// a.push(13); /* A-L */
+// a.pop(); /* D-L */
+// a.shift(); /* D-L */
+// a.unshift(15); /* A-L */
+// console.log(a);
+
+// a.splice(2,0,18); /* (index,no of ddeletions,insertion values) */
+// var k = a.slice(0,2); /*prining starting to end-1*/
+// console.log(k)
+// a.map(ele => {
+//     console.log(ele);
+// })
+
